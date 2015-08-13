f2c97925279a35f8fe432146877c03f90c35265c
John P <issio01@noreply.github.com>
2015-08-13 08:54:07 -0700

    Implemented JQuery and Tile Sliding (working) Next, will add a randomizer with sliding tiles and a fade in victory screen. Also will combine $(document).ready statements.

--- sliding_puzzle.js
+++ sliding_puzzle.js
@@ -1,5 +1,50 @@
 blank_position = 9;
 
+$(document).ready(function() {
+	$(".tile").click(function() {
+		move(this);
+	})
+})
+
+$(document).ready(function() {
+	$("#randomizer").click(function() {
+		randomize(500);
+	})
+})
+
+$(document).ready(function() {
+	$(".thumbnail").click(function(){
+		$("#top_left").attr("src", $(this).attr("src"));
+		$("#top_center").attr("src", $(this).attr("src"));
+		$("#top_right").attr("src", $(this).attr("src"));
+		$("#middle_left").attr("src", $(this).attr("src"));
+		$("#middle_center").attr("src", $(this).attr("src"));
+		$("#middle_right").attr("src", $(this).attr("src"));
+		$("#bottom_left").attr("src", $(this).attr("src"));
+		$("#bottom_center").attr("src", $(this).attr("src"));
+		$("#bottom_right").attr("src", $(this).attr("src"));
+	})
+})
+
+$(document).keydown(function(event) {
+	var key = event.which;
+	var tile_position;
+	if (key == 87) {
+		tile_position = blank_position - 3;
+	}
+	else if (key == 83) {
+		tile_position = blank_position + 3;
+	}
+	else if (key == 65) {
+		tile_position = blank_position - 1;
+	}
+	else if (key == 68) {
+		tile_position = blank_position + 1;
+	}
+	tile = getTile(tile_position);
+	move(tile);
+})
+
 function adjacent(position1, position2) {
 	if ((position1 == position2 + 1 && position1 != 4 && position1 != 7)
 		|| (position1 == position2 - 1 && position1 != 3 && position1 != 6)
@@ -26,56 +71,57 @@ function tileBetween(position1, position2) {
 	}	
 }
 
-function moveDirection(tile) {
-
+function getTilePosition(tile) {
+	return parseInt($(tile).attr("class").substring(14));
 }
 
-function slideOntoBlank(position) {
-	$(document).ready(function() {
-		var tile = $(".position_" + position)
-
-		if (position == blank_position + 1 && position != 4 && blank_position != 7) {
-			$(document).ready(function() {
-				tile.slideRight("slow", function() {
-					swapBlankFor(position)
-				})
-			})
-		}
-		else if	(position == blank_position - 1 && position != 3 && position != 6) {
-			return 'left';
-		}
-		else if (position == blank_position + 3) {
-			return 'down';
-		}
-		else if (position == blank_position - 3) {
-			return 'up';
-		}
+function getTile(position) {
+	return $(".position_" + position);
 }
 
 function swapBlankFor(position) {
-	$(document).ready(function(){
-		$(".position_" + position).attr("class", "tile position_" + blank_position);
-	})
+	var tile = getTile(position);
+	tile.attr("class", "tile position_" + blank_position);
 	blank_position = position;
 }
 
+function slideTileToBlank(position) {
+	var tile = getTile(position)
+	if (position == blank_position + 1 && position != 4 && position != 7) {
+		tile.animate({left:"-=169px"}, "fast");
+	}
+	else if	(position == blank_position - 1 && position != 3 && position != 6) {
+		tile.animate({left:"+=169px"}, "fast");
+	}
+	else if (position == blank_position + 3) {
+		tile.animate({top:"-=169px"}, "fast");
+	}
+	else if (position == blank_position - 3) {
+		tile.animate({top:"+=169px"}, "fast");
+	}
+}
+
 function move(div) {
-	var class_name = div.className;
-	var tile_position = parseInt(class_name.substring(14));
+	var tile_position = getTilePosition(div);
 	if (adjacent(tile_position, blank_position)) {
+		slideTileToBlank(tile_position);
 		swapBlankFor(tile_position);
 	}
 	else if (twoTilesAway(tile_position, blank_position)) {
 		var middle_position = tileBetween(tile_position, blank_position);
+		slideTileToBlank(middle_position);
 		swapBlankFor(middle_position);
+		slideTileToBlank(tile_position)
 		swapBlankFor(tile_position);
 	}
 }
 
+// Returns a Random Integer Between the min and max values (Both Inclusive)
 function getRandomIntegerBetween(min, max) {
 	return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
+// Randomizes the Tiles; ~30% chance that it will move a tile per each random number/iteration
 function randomize(n) {
 	for (var i = 0; i < n; i++) {
 		random_number = getRandomIntegerBetween(1, 9);
@@ -85,18 +131,12 @@ function randomize(n) {
 	}
 }
 
+// Checks to see if a position is within the tile positions 1 to 9
 function inRange(position) {
 	if (position >= 1 && position <= 9) {
 		return true;
 	}
 }
-
-
-
-
-
-
-
 
 
 
